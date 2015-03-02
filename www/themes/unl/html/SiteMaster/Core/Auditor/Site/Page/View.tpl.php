2a4271a953eae2436b1ef51076b88335e55f84f0
Michael Fairchild <mfairchild365@gmail.com>
2015-03-02 14:51:38 -0600

    Override the default theme to implement our grid system

---
+++ www/themes/unl/html/SiteMaster/Core/Auditor/Site/Page/View.tpl.php
@@ -0,0 +1,106 @@
+<?php
+$scan = $context->page->getScan();
+$metric_grades = $context->page->getMetricGrades();
+?>
+<header class="page-scan-header">
+    <div class="letter-grade-container">
+        <span class="letter-grade unknown"><?php echo $context->page->letter_grade ?></span>
+    </div>
+    <div class="details">
+        <span class="title">Page: <?php echo $context->page->getTitle(); ?></span>
+        <span class="page-url">
+            Scanned URL:
+            <?php echo $context->page->uri ?>
+        </span>
+    </div>
+</header>
+
+<div class="wdn-grid-set">
+    <div class="bp2-wdn-col-two-sevenths">
+        <section class="in-page-nav info-section">
+            <div>
+                <header>
+                    <h2>Scan Information</h2>
+                </header>
+                <ul>
+                    <li><span class="scan-status">Status: <?php echo $context->page->status ?></span></li>
+                    <li><span class="scanned-date">Scanned on: <?php echo $context->page->start_time ?></span></li>
+                </ul>
+            </div>
+            <div>
+                <header>
+                    <h2>Links</h2>
+                </header>
+                <ul>
+                    <li><a href="<?php echo $scan->getURL() ?>">Go back to the site scan</a></li>
+                    <li><a href="<?php echo $context->page->uri; ?>" target="_blank">Go to the page <img
+                                src="<?php echo \SiteMaster\Core\Config::get('URL') ?>www/images/external.png"
+                                alt="link to external site"/></a></li>
+                    <li><a href="<?php echo $context->getURL() . 'links-to-this/' ?>">View pages that link to this
+                            page</a></li>
+                </ul>
+                <?php echo $savvy->render($context->page->getScanForm()) ?>
+            </div>
+        </section>
+    </div>
+
+    <div class="bp2-wdn-col-five-sevenths">
+        <?php
+        if (!$context->page->isComplete()) {
+            ?>
+            <div class="panel notice">
+                The scan has not finished for this page yet. Refresh the page to get the most recent progress.
+            </div>
+        <?php
+        }
+        ?>
+
+        <section class="page-scan-content">
+            <header>
+                <h2>Metric Grades</h2>
+            </header>
+            <?php
+            echo $savvy->render($metric_grades);
+            ?>
+        </section>
+        <div class="page-scan-scoring">
+            Scoring
+            <table>
+                <thead>
+                <tr>
+                    <th>Metric</th>
+                    <th>Weighted Score</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                foreach ($metric_grades as $metric_grade) {
+                    $name = 'unknown';
+                    $metric_record = $metric_grade->getMetric();
+                    if ($metric_record && $metric_object = $metric_record->getMetricObject()) {
+                        $name = $metric_object->getName();
+                    }
+                    ?>
+                    <tr>
+                        <td><?php echo $name ?></td>
+                        <td><?php echo $metric_grade->weighted_grade ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+                </tbody>
+                <tfoot>
+                <tr>
+                    <td>
+                        Total:
+                    </td>
+                    <td class="total">
+                        <?php echo $context->page->point_grade ?>/<?php echo $context->page->points_available ?>
+                        = <?php echo $context->page->letter_grade ?>
+                    </td>
+                </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
+</div>
