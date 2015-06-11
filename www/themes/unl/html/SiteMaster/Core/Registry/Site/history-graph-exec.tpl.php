1ebf0ac10a9582a447561c48f25db006010c0471
Michael Fairchild <mfairchild365@gmail.com>
2015-06-11 09:18:05 -0500

    Implement the history graph so it will work with the framework

---
+++ www/themes/unl/html/SiteMaster/Core/Registry/Site/history-graph-exec.tpl.php
@@ -0,0 +1,16 @@
+<script>
+    require(['jquery', '<?php echo \SiteMaster\Core\Config::get('URL') . 'www/js/vendor/chart.min.js' ?>'], function($) {
+        var ctx = document.getElementById("history_chart").getContext("2d");
+        var chart = new Chart(ctx).Line(data, {
+            responsive: false,
+            maintainAspectRatio: false,
+            datasetFill: false,
+            legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span class=\"color\" style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+            tooltipFontSize: 10,
+            tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
+            multiTooltipTemplate: "<%if (datasetLabel){%><%=datasetLabel%>: <%}%><%= value %>",
+        });
+
+        $("#history_legend").html(chart.generateLegend());
+    });
+</script>
