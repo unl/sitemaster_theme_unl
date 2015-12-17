9705d693490490a4814e84e3a85e9f3f6b4ba97b
Michael Fairchild <mfairchild365@gmail.com>
2015-12-17 15:09:49 -0600

    Use new GA tracking code

--- www/themes/unl/html/SiteMaster/Core/localfooter.tpl.php
+++ www/themes/unl/html/SiteMaster/Core/localfooter.tpl.php
@@ -27,10 +27,12 @@
     </div>
 </div>
 
-<script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(["_setAccount", "UA-3203435-18"]); //replace with your unique tracker id
-    _gaq.push(["_setDomainName", ".unl.edu"]);
-    _gaq.push(["_setAllowLinker", true]);
-    _gaq.push(["_trackPageview"]);
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-3203435-18', 'auto');
+    ga('send', 'pageview');
 </script>
