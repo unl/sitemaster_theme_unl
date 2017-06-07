5da9206b0d52502ec06579bf33cb30c92e1693ed
Michael Fairchild <mfairchild365@gmail.com>
2017-06-07 13:03:00 -0500

    Don't link to the unl directory for non-unl users

--- www/themes/unl/html/SiteMaster/Core/Registry/Site/Member/Summary.tpl.php
+++ www/themes/unl/html/SiteMaster/Core/Registry/Site/Member/Summary.tpl.php
@@ -3,7 +3,12 @@ $user = $context->getUser();
 $roles = $context->getRoles();
 ?>
 <li>
-    <a href="https://directory.unl.edu/people/<?php echo $user->uid ?>"><?php echo $user->getName(); ?></a> (<?php echo $user->uid ?>)
+    <?php if ($user->provider === 'unl.edu'): ?>
+        <a href="https://directory.unl.edu/people/<?php echo $user->uid ?>"><?php echo $user->getName(); ?></a> (<?php echo $user->uid ?>)
+    <?php else: ?>
+        <?php echo $user->getName(); ?>
+    <?php endif; ?>
+    
     <?php if ($roles->count()): ?>
         <ul>
             <?php foreach ($roles as $role): ?>
