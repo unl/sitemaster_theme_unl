10323e79aadc8c4abaafd67bdd1249016f033c3c
Ryan Dee <ryan@skoolbus39.com>
2019-10-21 16:10:05 -0500

    Update local footer markup
    - Convert `<nav>` elements to `<div>` elements
    - Remove `aria-labelledby` from footer group 1
    - Move footer group 2 `aria-labelledby` to unordered list
    - Convert headings from `<h3>` to `<h2>`

--- www/themes/unl/html/SiteMaster/Core/localfooter.tpl.php
+++ www/themes/unl/html/SiteMaster/Core/localfooter.tpl.php
@@ -1,15 +1,15 @@
-<nav id="dcf-footer-group-1" role="navigation" aria-labelledby="dcf-footer-group-1-heading">
-  <h3 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-1-heading">About Web Audit</h3>
+<div id="dcf-footer-group-1">
+  <h2 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-1-heading">About Web Audit</h2>
   <p>This application is a product of the <a href="https://dxg.unl.edu/">Digital Experience Group at Nebraska</a>. DXG is a partnership of <a href="https://ucomm.unl.edu/">University Communication</a> and <a href="https://its.unl.edu/">Information Technology Services</a>.</p>
   <p>Web Audit is administrated by the <a href="https://wdn.unl.edu/shared-governance-board">WDN Governance Board</a>.</p>
-</nav>
-<nav id="dcf-footer-group-2" role="navigation" aria-labelledby="dcf-footer-group-2-heading">
-  <h3 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-2-heading">Related Links</h3>
-  <ul class="dcf-list-bare dcf-mb-0">
+</div>
+<div id="dcf-footer-group-2">
+  <h2 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-2-heading">Related Links</h2>
+  <ul aria-labelledby="dcf-footer-group-2-heading">
     <li><a href="<?php echo \SiteMaster\Core\Config::get('URL') ?>terms-of-use/">Terms Of Use</a></li>
     <li><a href="https://wdn.unl.edu/">Web Developer Network</a></li>
     <li><a href="https://dxg.unl.edu/">Digital Experience Group</a></li>
     <li><a href="https://wdn.unl.edu/documentation/unl-webaudit">Documentation</a></li>
     <li><a href="https://wdn.unl.edu/documentation/unl-webaudit/changelog">Changelog</a></li>
   </ul>
-</nav>
+</div>
