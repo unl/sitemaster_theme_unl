702ec7ba401bef4ffdc9468050f527301fe96a43
Jeff Sturek <jsturek@gmail.com>
2020-03-18 14:13:41 -0500

    Implement 5.1 (#17)
    * Implement 5.1 WDN Framework
    
    * Use Local template
    
    Co-authored-by: Jeff Sturek <jsturek8@unl.edu>

--- www/themes/unl/html/SiteMaster/Core/Controller.tpl.php
+++ www/themes/unl/html/SiteMaster/Core/Controller.tpl.php
@@ -1,10 +1,10 @@
 <?php
 use UNL\Templates\Templates;
 
-$page = Templates::factory('Fixed', Templates::VERSION_5);
+$page = Templates::factory('Local', Templates::VERSION_5_1);
 
 $wdn_include_path = \SiteMaster\Core\Util::getRootDir();
-if (file_exists($wdn_include_path . '/wdn/templates_5.0')) {
+if (file_exists($wdn_include_path . '/wdn/templates_5.1')) {
     $page->setLocalIncludePath($wdn_include_path);
 }
 
