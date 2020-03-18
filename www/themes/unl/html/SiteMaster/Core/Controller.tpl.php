29ad5d7da617ad0aeb9b4f9c4f8704bfc97ed159
Jeff Sturek <jsturek@gmail.com>
2020-03-18 15:41:40 -0500

    Update 5.1 deprecated css reference

--- www/themes/unl/html/SiteMaster/Core/Controller.tpl.php
+++ www/themes/unl/html/SiteMaster/Core/Controller.tpl.php
@@ -18,7 +18,7 @@ $page->pagetitle = '<h1>' . $context->output->getPageTitle() . '</h1>';
 $page->affiliation = '';
 
 // Add WDN Deprecated Styles
-$page->head .= '<link rel="preload" href="https://unlcms.unl.edu/wdn/templates_5.0/css/deprecated.css" as="style" onload="this.onload=null;this.rel=\'stylesheet\'"> <noscript><link rel="stylesheet" href="https://unlcms.unl.edu/wdn/templates_5.0/css/deprecated.css"></noscript>';
+$page->head .= '<link rel="preload" href="/wdn/templates_5.1/css/deprecated.css" as="style" onload="this.onload=null;this.rel=\'stylesheet\'"> <noscript><link rel="stylesheet" href="/wdn/templates_5.1/css/deprecated.css"></noscript>';
 
 //Navigation
 $page->breadcrumbs  = '
