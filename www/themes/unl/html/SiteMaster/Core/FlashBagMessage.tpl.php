7f166775e48c6a065d9d21edde28898364e5e277
Jeff Sturek <jsturek@gmail.com>
2021-11-05 14:34:05 -0500

    UPdate notices

--- www/themes/unl/html/SiteMaster/Core/FlashBagMessage.tpl.php
+++ www/themes/unl/html/SiteMaster/Core/FlashBagMessage.tpl.php
@@ -1,24 +1,21 @@
 <?php
-$type = '';
+$type = 'dcf-notice-info';
 $title = 'Info';
 switch ($context->type) {
     case \SiteMaster\Core\FlashBagMessage::TYPE_ERROR:
-        $type = 'negate';
+        $type = 'dcf-notice-danger';
         $title = 'ERROR';
         break;
     case \SiteMaster\Core\FlashBagMessage::TYPE_SUCCESS:
-        $type = 'affirm';
+        $type = 'dcf-notice-success';
         $title = 'SUCCESS';
         break;
 }
 ?>
 
-<div class="wdn_notice <?php echo $type ?>">
-    <div class="close">
-        <a href="#" title="Close this notice">Close this notice</a>
-    </div>
-    <div class="message">
-        <h4><?php echo $title ?></h4>
+<div class="dcf-notice <?php echo $type ?>" hidden>
+    <h2><?php echo $title ?></h2>
+    <div>
         <p><?php echo $context->message ?></p>
     </div>
 </div>
--- www/themes/unl/html/SiteMaster/Core/ValidationMessage.tpl.php
+++ www/themes/unl/html/SiteMaster/Core/ValidationMessage.tpl.php
@@ -1,10 +1,6 @@
-
-<div class="wdn_notice negate">
-    <div class="close">
-        <a href="#" title="Close this notice">Close this notice</a>
-    </div>
-    <div class="message">
-        <h4>Validation Error</h4>
+<div class="dcf-notice dcf-notice-danger" hidden>
+    <h2>Validation Error</h2>
+    <div>
         <ul>
         <?php foreach ($context->messages as $element=>$message): ?>
             <li><a href="#<?php echo $element ?>"><?php echo $message ?></a></li>
