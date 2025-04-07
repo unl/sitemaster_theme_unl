02276ee19d9cb2d97a6d7da1a819111c0c822698
Thomas Neumann <thomasneumann21800@gmail.com>
2025-04-07 09:44:05 -0500

    Update WDN link

--- www/themes/unl/html/SiteMaster/Core/localfooter.tpl.php
+++ www/themes/unl/html/SiteMaster/Core/localfooter.tpl.php
@@ -1,7 +1,7 @@
 <div id="dcf-footer-group-1">
   <h2 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-1-heading">About Web Audit</h2>
   <p>This application is a product of the <a href="https://dxg.unl.edu/">Digital Experience Group</a> at <a href="https://www.unl.edu/">Nebraska</a>. DXG is part of <a href="https://ucomm.unl.edu/">University Communication &amp; Marketing</a>.</p>
-  <p>Web Audit is administrated by the <a href="https://wdn.unl.edu/shared-governance-board">WDN Governance Board</a>.</p>
+  <p>Web Audit is administrated by the <a href="https://wdn.unl.edu/governance-board/">WDN Governance Board</a>.</p>
 </div>
 <div id="dcf-footer-group-2">
   <h2 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-2-heading">Related Links</h2>
@@ -9,7 +9,6 @@
     <li><a href="<?php echo \SiteMaster\Core\Config::get('URL') ?>terms-of-use/">Terms Of Use</a></li>
     <li><a href="https://wdn.unl.edu/">Web Developer Network</a></li>
     <li><a href="https://dxg.unl.edu/">Digital Experience Group</a></li>
-    <li><a href="https://wdn.unl.edu/documentation/unl-webaudit">Documentation</a></li>
-    <li><a href="https://wdn.unl.edu/documentation/unl-webaudit/changelog">Changelog</a></li>
+    <li><a href="https://wdn.unl.edu/public-info-apps-documentation/web-audit/">Documentation</a></li>
   </ul>
 </div>
